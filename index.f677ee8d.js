acd5653b317d208d8dd1a40849d51f1063a5b29c
AVerba <57949955+AVerba@users.noreply.github.com>
2023-09-11 10:52:58 +0000

    Deploying to gh-pages from @ AVerba/art-collection@0bd46234f09147b0c1fd573cd391a5e188fb7df3 🚀

--- index.f677ee8d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".js-open-menu"),t=document.querySelector(".js-close-menu"),c=document.querySelector(".js-menu-container"),n=()=>{const t="true"===e.getAttribute("aria-expanded")||!1;e.setAttribute("aria-expanded",!t),c.classList.toggle("is-open")};e.addEventListener("click",n),t.addEventListener("click",n);const s=document.querySelectorAll(".products__item");s.forEach((e=>{e.addEventListener("click",(()=>{s.forEach((e=>{e.classList.remove("_active")})),e.classList.add("_active")}))}));
-//# sourceMappingURL=index.f677ee8d.js.map
